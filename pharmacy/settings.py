4678c8edf416956e12bc5fe90e937c2d583a1c29
= <=>
2021-08-27 00:12:32 -0600

    heroku bug fix

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 STATIC_URL = '/static/'
 
